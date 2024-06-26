fe6be9b51227c6b1a9f513148259590990841bdb
PrimeX1 <kiritosinon9@gmail.com>
2024-06-26 23:11:21 +0700

    feat( UserController ): store , email verify,lupa password, lupa password verify, send mail

---
+++ resources/views/mail/reset_password.blade.php
@@ -0,0 +1,14 @@
+<div>Halo {{$otp['user']}},<div>
+
+<div>Kami menerima permintaan untuk mengatur ulang password akun Anda. Untuk melanjutkan proses reset password, silakan masukkan kode verifikasi (OTP) berikut:</div>
+
+<h4 style="margin: 1rem 0rem;"><strong>{{$otp['otp']}}</strong></h4>
+
+<div>Kode ini akan berlaku selama 10 menit. Jika Anda tidak meminta kode ini, harap abaikan email ini.</div>
+
+<div>Jika Anda memiliki pertanyaan atau membutuhkan bantuan, jangan ragu untuk menghubungi tim dukungan kami.</div>
+
+<div style="margin-top: 0.5rem;">
+    <div>Terima kasih,</div>
+    <div>Tim PT Mencari Cinta Sejati</div>
+</div>
